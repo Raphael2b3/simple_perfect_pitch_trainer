218feb5af4ebb4dd161fb2bbd34a8177a1867594
Raphael Schütz <70175424+Raphael2b3@users.noreply.github.com>
2025-02-02 16:55:45 +0100

    fixed everything

---
+++ lib/services/chord_player/chord_player_controller.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chord_player_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chordPlayerControllerHash() =>
+    r'547d272035a7d7993b23d2a407c3bfd6df48ffff';
+
+/// See also [ChordPlayerController].
+@ProviderFor(ChordPlayerController)
+final chordPlayerControllerProvider = AutoDisposeAsyncNotifierProvider<
+  ChordPlayerController,
+  ChordPlayer
+>.internal(
+  ChordPlayerController.new,
+  name: r'chordPlayerControllerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$chordPlayerControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ChordPlayerController = AutoDisposeAsyncNotifier<ChordPlayer>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/services/ui_state_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ui_state_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$uiStateControllerHash() => r'a686834bb6d7772b781efdf9ab230e39a4e1f887';
+
+/// See also [UiStateController].
+@ProviderFor(UiStateController)
+final uiStateControllerProvider =
+    AutoDisposeNotifierProvider<UiStateController, UIState>.internal(
+      UiStateController.new,
+      name: r'uiStateControllerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$uiStateControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$UiStateController = AutoDisposeNotifier<UIState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
