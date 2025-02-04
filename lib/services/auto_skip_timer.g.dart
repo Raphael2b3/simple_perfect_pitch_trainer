7f42909536571d3cf97b1f9bbbf040d0a56034e0
Raphael Schütz <70175424+Raphael2b3@users.noreply.github.com>
2025-02-04 17:21:20 +0100

    encountered insane bug??? WTF

---
+++ lib/services/auto_skip_timer.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auto_skip_timer.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$autoSkipTimerHash() => r'8a9d4ea1f66b4113834c2aace12e2d8e0de799b7';
+
+/// See also [AutoSkipTimer].
+@ProviderFor(AutoSkipTimer)
+final autoSkipTimerProvider =
+    AutoDisposeNotifierProvider<AutoSkipTimer, Timer?>.internal(
+      AutoSkipTimer.new,
+      name: r'autoSkipTimerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$autoSkipTimerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$AutoSkipTimer = AutoDisposeNotifier<Timer?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
